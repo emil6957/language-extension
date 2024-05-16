52ec5cffe280aebe3d5f004934086994ee2d1b9b
Emil <emiliano.milewski@gmail.com>
2024-05-16 23:34:06 +0100

    sending message from content to service

--- extension/content.js
+++ extension/content.js
@@ -1,29 +1,39 @@
-function showPopup() {
-    const styles = `
-        position: absolute;
-        top: 0;
-        left: 0;
-        background-color: red;
-        width: 15rem;
-        height: 15rem;
-        z-index: 9999;
-    `
+const styles = `
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: red;
+    width: 15rem;
+    height: 15rem;
+    z-index: 9999;
+`
 
-    const html = `
-        <div style="${styles}">
-            <p>POPUP CONTENT DIV</p>
-        </div>
-    ` 
+const html = `
+    <div style="${styles}">
+        <p>POPUP CONTENT DIV</p>
+    </div>
+` 
 
-    const popupDivContainer = document.createElement("div");
-    popupDivContainer.setAttribute("id", "language-popup")
-    popupDivContainer.innerHTML = html;
-    document.documentElement.appendChild(popupDivContainer);
-}
+const popupDivContainer = document.createElement("div");
+popupDivContainer.setAttribute("id", "language-popup")
+popupDivContainer.innerHTML = html;
 
-chrome.runtime.onMessage.addListener((request) => {
-        console.log("Request");
-        console.log(request);
-        showPopup();
+// Loads and sends request to load popup
+(async () => {
+    console.log("SENDING REQUEST");
+    const response = await chrome.runtime.sendMessage("showPopup");
+    console.log(response);
+    if (response === true) {
+        document.documentElement.appendChild(popupDivContainer);
     }
-);
+})();
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log("REQUEST IN CONTENT");
+        console.log(request);
+        console.log("Sender tab url");
+        console.log(sender.tab.url)
+        sendResponse("content sending message back from listenr");
+    }
+)
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -7,10 +7,24 @@ chrome.runtime.onInstalled.addListener(async () => {
     });
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-        chrome.runtime.sendMessage("show-popup", (response) => {
-            console.log("REPONSE");
-            console.log(response);
-        });
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendReponse) {
+        console.log("Request");
+        console.log(request);
+        console.log("Sender");
+        console.log(sender);
+        console.log("sent response");
+        sendReponse(true);
     }
 );
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    }
+);
+
+(async () => {
+    console.log("RUNNING FUNCTION IN SERVICE WORKER");
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    const response = await chrome.tabs.sendMessage(tab.id, "testing message");
+    console.log(response);
+})();
