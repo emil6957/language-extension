71d90f4762f7ca954621d005411c5d0349006061
Emil <emiliano.milewski@gmail.com>
2024-06-01 00:25:16 +0100

    messages are now sure to be sent and popup shows up after clicking context menu

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,6 @@
 const styles = `
+    display: none;
     position: absolute;
-    top: 0;
-    left: 0;
     background-color: red;
     width: 15rem;
     height: 15rem;
@@ -9,7 +8,7 @@ const styles = `
 `
 
 const html = `
-    <div style="${styles}">
+    <div id="language-popup-content" style="${styles}">
         <p>POPUP CONTENT DIV</p>
     </div>
 ` 
@@ -17,23 +16,70 @@ const html = `
 const popupDivContainer = document.createElement("div");
 popupDivContainer.setAttribute("id", "language-popup")
 popupDivContainer.innerHTML = html;
+document.documentElement.appendChild(popupDivContainer);
 
 // Loads and sends request to load popup
-(async () => {
-    console.log("SENDING REQUEST");
-    const response = await chrome.runtime.sendMessage("showPopup");
-    console.log(response);
-    if (response === true) {
-        document.documentElement.appendChild(popupDivContainer);
+// (async () => {
+//     console.log("SENDING REQUEST");
+//     const response = await chrome.runtime.sendMessage("showPopup");
+//     console.log(response);
+//     if (response === true) {
+//         document.documentElement.appendChild(popupDivContainer);
+//     }
+// })();
+
+let mousePos = { x: 0, y: 0 };
+document.onmousedown = function(e) {
+    const popupDivContent = document.getElementById("language-popup-content");
+
+    if(e.button === 0 || e.button === 2) {
+        console.log("CLICK");
+        console.log(e);
+        if(!popupDivContent.contains(e.target)) {
+            console.log("HIDDING POPUP");
+            popupDivContent.style.display = "none";
+        }
     }
-})();
+
+    if(e.button === 2) {
+        console.log("right click");
+        console.log(e);
+        mousePos.x = e.pageX;
+        mousePos.y = e.pageY;
+        const content = popupDivContainer.firstChild;
+        console.log(popupDivContainer);
+        console.log(popupDivContent);
+        console.log(content);
+        console.log(html);
+        popupDivContent.style.top = `${mousePos.y + 10}px`;
+        popupDivContent.style.left = `${mousePos.x}px`;
+    }
+}
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log("REQUEST IN CONTENT");
+        const popupDivContent = document.getElementById("language-popup-content");
+        if(request.ping) {
+            console.log("sending pong");
+            sendResponse({pong: true});
+            return;
+        }
+
+        if(request.showPopup) {
+            console.log("showing popup");
+            popupDivContent.style.display = "block";
+            sendResponse({showPopup: true, mousePos: mousePos});
+            return;
+        }
+
+        if(request.selectionText) {
+            console.log("setting selection text");
+            const content = document.createElement("p");
+            content.textContent = request.selectionText;
+            popupDivContent.append(content);
+        }
+
+        console.log("--- New message received ---");
         console.log(request);
-        console.log("Sender tab url");
-        console.log(sender.tab.url)
-        sendResponse("content sending message back from listenr");
     }
-)
+);
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,3 +1,24 @@
+function ensureSendMessage(tabId, message, callback) {
+    console.log("sending ping");
+    chrome.tabs.sendMessage(tabId, {ping: true}, function(response) {
+        console.log("ping sent");
+        if(response && response.pong) {
+            console.log("pong sent back");
+            chrome.tabs.sendMessage(tabId, message, callback);
+        } else {
+            console.log("INJECTING CONTENT SCRIPT");
+            chrome.tabs.executeScript(tabId, {file: "content.js"}, function() {
+                if(chrome.runtime.lastError) {
+                    console.error(chrome.runtime.lastError);
+                    throw Error("unable to inject script into tab " + tabId);
+                }
+                console.log("SENDING MESSAGE NOW");
+                chrome.tabs.sendMessage(tabId, message, callback);
+            });
+        }
+    });
+}
+
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.contextMenus.create({
         id: "openPopup",
@@ -6,6 +27,13 @@ chrome.runtime.onInstalled.addListener(async () => {
         contexts: ["selection"]
     });
 });
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+        console.log("INFO");
+        console.log(info.selectionText);
+        ensureSendMessage(tab.id, {showPopup: true, selectionText: info.selectionText});
+    }
+);
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendReponse) {
@@ -17,14 +45,3 @@ chrome.runtime.onMessage.addListener(
         sendReponse(true);
     }
 );
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    }
-);
-
-(async () => {
-    console.log("RUNNING FUNCTION IN SERVICE WORKER");
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, "testing message");
-    console.log(response);
-})();
